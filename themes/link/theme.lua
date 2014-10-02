49fbb078d7566d67a513636d1b7b17c75bd67f48
Lukas Geis <mail.blueluke@yahoo.de>
2014-10-02 20:46:44 +0200

    Link theme: theme.lua sorted

--- themes/link/theme.lua
+++ themes/link/theme.lua
@@ -10,40 +10,72 @@ theme                                           = {}
 theme.dir                                       = os.getenv("HOME") .. "/.config/awesome/themes/link"
 theme.wallpaper                                 = theme.dir .. "/wall.png"
 
+-- {{{ Fonts
+
 theme.font                                      = "Dejavu Sans Bold 10"
-theme.fg_normal                                 = "#343d46"--"#BBBBBB"
-theme.fg_focus                                  = "#2b303b" --#eff1f5"
-theme.bg_normal                                 = "#dfe1e8"--"#111111"
-theme.bg_focus                                  = "#dfe1e8"--"#dfe1e8" --"#111111"
+theme.taglist_font                  = "Tamsyn 7"
+
+-- }}}
+
+-- {{{ Colors
+
+theme.fg_normal                                 = "#343d46"
+theme.fg_focus                                  = "#2b303b"
+theme.bg_normal                                 = "#dfe1e8"
+theme.bg_focus                                  = "#dfe1e8"
 theme.fg_urgent                                 = "#bf616a"
-theme.bg_urgent                                 = "#dfe1e8" --"#111111"
-theme.border_width                              = 2
+theme.bg_urgent                                 = "#dfe1e8"
 theme.border_normal                             = "#2b303b"
-theme.border_focus                              = "#343d46"--"#93B6FF"
+theme.border_focus                              = "#343d46"
+
+-- }}}
+
+-- {{{ Misc
+
+theme.border_width                              = 2
+theme.useless_gap_width                         = 10
 
 theme.menu_height                               = "15"
 theme.menu_width                                = "150"
 
 theme.tasklist_disable_icon                     = true
 
-theme.awesome_icon                              = theme.dir .."/icons/awesome.png"
-theme.menu_submenu_icon                         = theme.dir .."/icons/submenu.png"
-theme.widget_bg                                 = theme.dir .. "/icons/widget_bg.png"
+-- }}}
+
+-- {{{ Widget Icons
+
+-- Volume
 theme.vol                                       = theme.dir .. "/icons/vol.png"
 theme.vol_low                                   = theme.dir .. "/icons/vol_low.png"
 theme.vol_no                                    = theme.dir .. "/icons/vol_no.png"
 theme.vol_mute                                  = theme.dir .. "/icons/vol_mute.png"
-theme.disk                                      = theme.dir .. "/icons/disk.png"
+
+-- Battery
 theme.ac                                        = theme.dir .. "/icons/ac.png"
 theme.bat                                       = theme.dir .. "/icons/bat.png"
+theme.bat_medium                          = theme.dir .. "/icons/bat_medium.png"
 theme.bat_low                                   = theme.dir .. "/icons/bat_low.png"
 theme.bat_empty                                 = theme.dir .. "/icons/bat_empty.png"
+
+-- MPD
 theme.play                                      = theme.dir .. "/icons/play.png"
 theme.pause                                     = theme.dir .. "/icons/pause.png"
+
+-- Redshift
 theme.rs_on                                     = theme.dir .. "/icons/redshift_on.png"
 theme.rs_off                                    = theme.dir .. "/icons/redshift_off.png"
+
+-- Misc
 theme.pacman                                    = theme.dir .. "/icons/pacman.png"
 
+-- Supported Layouts
+theme.layout_floating                           = theme.dir .. "/icons/floating.png"
+theme.layout_uselessfair                        = theme.dir .. "/icons/fairv.png"
+theme.layout_uselesspiral                       = theme.dir .. "/icons/dwindle.png"
+theme.layout_uselesstileright                  = theme.dir .. "/icons/tile.png"
+theme.layout_uselesstilebottom                  = theme.dir .. "/icons/tileb.png"
+
+-- Unused layouts - Icons may be missing
 theme.layout_tile                               = theme.dir .. "/icons/tile.png"
 theme.layout_tileleft                           = theme.dir .. "/icons/tileleft.png"
 theme.layout_tilebottom                         = theme.dir .. "/icons/tilebottom.png"
@@ -55,28 +87,22 @@ theme.layout_dwindle                            = theme.dir .. "/icons/dwindle.p
 theme.layout_max                                = theme.dir .. "/icons/max.png"
 theme.layout_fullscreen                         = theme.dir .. "/icons/fullscreen.png"
 theme.layout_magnifier                          = theme.dir .. "/icons/magnifier.png"
-theme.layout_floating                           = theme.dir .. "/icons/floating.png"
-
 theme.layout_centerfair                         = theme.dir .. "/icons/centerfair.png"
 theme.layout_termfair                           = theme.dir .. "/icons/termfair.png"
-theme.layout_uselessfair                        = theme.dir .. "/icons/fairv.png"
 theme.layout_uselessfairh                       = theme.dir .. "/icons/fairh.png"
 theme.layout_uselessdwindle                     = theme.dir .. "/icons/dwindle.png"
 theme.layout_uselesstile                        = theme.dir .. "/icons/tile.png"
-theme.layout_uselesspiral                       = theme.dir .. "/icons/dwindle.png"
-theme.layout_uselesstileright                  = theme.dir .. "/icons/tile.png"
-theme.layout_uselesstilebottom                  = theme.dir .. "/icons/tileb.png"
 
+-- }}}
 
--- Taglist icons
+-- {{{ Taglist
 
-theme.taglist_font                  = "Tamsyn 7"
 theme.taglist_bg_empty = "png:/" .. theme.dir .. "/icons/empty.png"
 theme.taglist_bg_occupied = "png:/" .. theme.dir .. "/icons/occupied.png"
 theme.taglist_bg_urgent = "png:/" .. theme.dir .. "/icons/urgent.png"
 theme.taglist_bg_focus = "png:/" .. theme.dir .. "/icons/focus.png"
 
--- lain related
-theme.useless_gap_width                         = 10
+-- }}}
+
 
 return theme
